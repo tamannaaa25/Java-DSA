a4da8bfac15710e668b66e6866ec6f01b6ecc34c
tamannaaa25 <tamanna.agy@gmail.com>
2025-02-06 19:46:40 +0530

    Add files via upload

---
+++ day10.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class day10 {
+
+    // FIBONACCI SERIES
+    /* 
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n));
+    }
+
+    static int fibonacci(int n){
+        if(n<2){
+            return n;
+        }else{
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+
+    */
+
+
+    
+}
